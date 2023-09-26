1ea3781f3ec9405db88f41cd20887bd810ad6ec7
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-09-26 03:07:40 -0500

    added class list

---
+++ src/components/Banner.jsx
@@ -0,0 +1,5 @@
+export const Banner = ({ title }) => {
+    return (
+        <h1>{title}</h1>
+    );
+};
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,9 @@
+export const CourseList = ({ courses }) => {
+    return (
+        <div>
+            {Object.entries(courses).map(([id, course]) =>
+                <p key={id}>{course.term} CS{course.number}: {course.title}</p>)}
+        </div>
+
+    );
+};
